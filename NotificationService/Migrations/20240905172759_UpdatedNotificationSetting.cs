04c8822e1c8cb15ca551258b0e9da5e86cd9a59b
ivanastevanovic-servalit <67589926+ivanaste@users.noreply.github.com>
2024-09-05 22:33:40 +0200

    Feature 17: Add notification details for reservation response

---
+++ NotificationService/Migrations/20240905172759_UpdatedNotificationSetting.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace NotificationService.Migrations
+{
+    /// <inheritdoc />
+    public partial class UpdatedNotificationSetting : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "IsReservationResponseNotificationEnabled",
+                table: "NotificationSettings",
+                newName: "IsReservationRejectedNotificationEnabled");
+
+            migrationBuilder.RenameColumn(
+                name: "IsReservationCancellationNotificationEnabled",
+                table: "NotificationSettings",
+                newName: "IsReservationDeletedNotificationEnabled");
+
+            migrationBuilder.AddColumn<bool>(
+                name: "IsReservationCancelledNotificationEnabled",
+                table: "NotificationSettings",
+                type: "boolean",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "IsReservationConfirmedNotificationEnabled",
+                table: "NotificationSettings",
+                type: "boolean",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "IsReservationCancelledNotificationEnabled",
+                table: "NotificationSettings");
+
+            migrationBuilder.DropColumn(
+                name: "IsReservationConfirmedNotificationEnabled",
+                table: "NotificationSettings");
+
+            migrationBuilder.RenameColumn(
+                name: "IsReservationRejectedNotificationEnabled",
+                table: "NotificationSettings",
+                newName: "IsReservationResponseNotificationEnabled");
+
+            migrationBuilder.RenameColumn(
+                name: "IsReservationDeletedNotificationEnabled",
+                table: "NotificationSettings",
+                newName: "IsReservationCancellationNotificationEnabled");
+        }
+    }
+}
