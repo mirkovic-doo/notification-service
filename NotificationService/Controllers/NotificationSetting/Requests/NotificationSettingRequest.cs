a14e6289fa86eb0c86c13759723da9cb815afb51
banzg00 <63750104+banzg00@users.noreply.github.com>
2024-09-05 23:36:05 +0200

    Feature 17: Add notification details for reservation response (#18) (#19)
    Co-authored-by: ivanaste <67589926+ivanaste@users.noreply.github.com>

--- NotificationService/Controllers/NotificationSetting/Requests/NotificationSettingRequest.cs
+++ NotificationService/Controllers/NotificationSetting/Requests/NotificationSettingRequest.cs
@@ -4,7 +4,9 @@ public record NotificationSettingRequest
 {
     public required Guid Id { get; set; }
     public bool IsReservationRequestNotificationEnabled { get; set; } = true;
-    public bool IsReservationResponseNotificationEnabled { get; set; } = true;
-    public bool IsReservationCancellationNotificationEnabled { get; set; } = true;
+    public bool IsReservationConfirmedNotificationEnabled { get; set; } = true;
+    public bool IsReservationRejectedNotificationEnabled { get; set; } = true;
+    public bool IsReservationDeletedNotificationEnabled { get; set; } = true;
+    public bool IsReservationCancelledNotificationEnabled { get; set; } = true;
     public bool IsReviewRecievedNotificationEnabled { get; set; } = true;
 }
