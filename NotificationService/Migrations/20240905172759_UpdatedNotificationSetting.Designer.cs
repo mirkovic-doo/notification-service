fd4ff4a1b4057d9a929dbe9c3dceeeb6cead1cc8
ivanaste <67589926+ivanaste@users.noreply.github.com>
2024-09-05 23:24:31 +0200

    Feature 17: Add notification details for reservation response (#18)

--- NotificationService/Migrations/20240905172759_UpdatedNotificationSetting.Designer.cs
+++ NotificationService/Migrations/20240905172759_UpdatedNotificationSetting.Designer.cs
@@ -2,6 +2,7 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using NotificationService.Infrastructure;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
@@ -11,9 +12,11 @@ using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 namespace NotificationService.Migrations
 {
     [DbContext(typeof(NotificationDbContext))]
-    partial class NotificationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20240905172759_UpdatedNotificationSetting")]
+    partial class UpdatedNotificationSetting
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
